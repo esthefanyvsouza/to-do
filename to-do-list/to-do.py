701c656bb54782f1b5c127ba090043b84cbc116b
Esthefany Souza <sttehvss@gmail.com>
2026-09-10 15:26:31 -0300

    mostrar menu

--- to-do-list/to-do.py
+++ to-do-list/to-do.py
@@ -2,3 +2,7 @@ tarefas = []
 def mostrar_menu ():
     print("/n===== LISTA DE TAREFAS =====")
     print("1. Adicionar Tarefa")
+    print("2. Exibir tarefas")
+    print("3. Mudar status da tarefa")
+    print("4. Apagar Tarefa")
+    print("5. Sair")

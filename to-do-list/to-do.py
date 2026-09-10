649528535f796448387a81add3ec0ce493216486
Esthefany Souza <sttehvss@gmail.com>
2026-09-10 16:56:47 -0300

    exibir tarefa

--- to-do-list/to-do.py
+++ to-do-list/to-do.py
@@ -3,7 +3,7 @@ tarefas = []
 status = ["a fazer", "fazendo", "concluida"]
 
 def mostrar_menu ():
-    print("/n===== LISTA DE TAREFAS =====")
+    print("\n===== LISTA DE TAREFAS =====")
     print("1. Adicionar Tarefa")
     print("2. Exibir tarefas")
     print("3. Mudar status da tarefa")
@@ -14,3 +14,11 @@ def adiconar_tarefa ():
     tarefa = input("Insira a tarefa: ")
     tarefas.append({"tarefa": tarefa, "status": "a fazer"})
     print(f"Tarefa '{tarefa}' adicionada!")
+
+def exibir_tarefa ():
+    if not tarefas:
+        print("Você não tem tarefas ainda.")
+        return
+    print("\nSuas tarefas: ")           
+    for index, tarefa in enumerate(tarefas, start=1):
+        print(f"{index}. {tarefa['tarefa']}")

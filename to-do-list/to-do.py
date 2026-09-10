b5c8bb8ea0ba6ec786caa522815eb813c8994218
Esthefany Souza <sttehvss@gmail.com>
2026-09-10 16:17:30 -0300

    adicionar tarefa

--- to-do-list/to-do.py
+++ to-do-list/to-do.py
@@ -9,4 +9,8 @@ def mostrar_menu ():
     print("3. Mudar status da tarefa")
     print("4. Apagar Tarefa")
     print("5. Sair")
-    
+
+def adiconar_tarefa ():
+    tarefa = input("Insira a tarefa: ")
+    tarefas.append({"tarefa": tarefa, "status": "a fazer"})
+    print(f"Tarefa '{tarefa}' adicionada!")
